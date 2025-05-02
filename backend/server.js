dede25c351444e350e04bc7a4eff29a095d30a2a
Wessel Dankers <wsl@fruit.je>
2025-05-02 16:17:43 +0200

    it compiles

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,18 @@ import http from 'http';
 import https from 'https';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import express from 'express';
+import Fastify from 'fastify';
+import { fastifySession } from '@fastify/session';
+import { fastifyCookie } from '@fastify/cookie';
+import fastifyCORS from '@fastify/cors';
+import fastifyFormBody from '@fastify/formbody';
+import fastifyFileUpload from 'fastify-file-upload';
+import MongoStore from 'connect-mongo';
 import mongoose from 'mongoose';
-import mongodbsession from 'express-mongodb-session';
-import session from 'express-session';
-import cors from 'cors';
-import multer from 'multer';
 import AdmZip from 'adm-zip';
-import fs from 'fs';
+import { existsSync, readFileSync, writeFileSync, mkdirSync, rmSync } from 'fs';
 import child_process from 'child_process';
-
+import { randomBytes } from 'crypto';
 import { LogModel } from './db/log.js';
 import { ProjectModel } from './db/project.js';
 import { UserModel } from './db/user.js';
@@ -24,9 +26,72 @@ import {
   TilBotProjectNotFoundError,
 } from './errors.js';
 
+// Generate a somewhat persistent token:
+function getOrCreateToken(tokenPath) {
+  try {
+    return readFileSync(tokenPath, { encoding: 'utf8' });
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      // File does not exist, generate token and save
+      const token = randomBytes(32).toString('base64');
+      writeFileSync(tokenPath, token, { encoding: 'utf8' });
+      return token;
+    } else {
+      // Rethrow any other error
+      throw error;
+    }
+  }
+}
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const upload = multer({ dest: 'tmp_upload/' });
+
+// Set up the MongoDB connection
+const dbPath = process.env.MONGO_USERNAME
+  ? `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@mongo:${process.env.MONGO_PORT ?? 27017}/${process.env.MONGO_DB ?? 'tilbot'}`
+  : (process.env.MONGO_DB ?? 'mongodb://127.0.0.1:27017/tilbot');
+
+await mongoose.connect(dbPath, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = Fastify({ logger: true });
+
+app.register(fastifyCookie, {
+  secret: process.env.COOKIE_SECRET || getOrCreateToken('/tmp/cookie-secret'),
+  maxAge: 60 * 60 * 24 * 7, // 1 week
+  secure: Boolean(process.env.HTTPS),
+  httpOnly: true,
+  sameSite: 'none',
+});
+
+app.register(fastifySession, {
+  secret: process.env.SESSION_SECRET || getOrCreateToken('/tmp/session-secret'),
+  store: MongoStore.create({
+    client: mongoose.connection.getClient(),
+    stringify: false,
+    autoRemoveInterval: 600, // minutes
+  }),
+});
+
+// For CORS
+app.register(fastifyCORS, {
+  allowedHeaders: ['Content-Type'],
+  origin: 'http://localhost:5173',
+  preflightContinue: true,
+  credentials: true,
+});
+
+// parse application/x-www-form-urlencoded
+app.register(fastifyFormBody);
+
+// File uploads
+app.register(fastifyFileUpload, {
+  safeFileNames: true,
+  abortOnLimit: true,
+  useTempFiles: true,
+  tempFileDir: '/tmp',
+});
+
+// Keep track of running external processes for bots.
+const running_bots = {};
 
 function start_bot(projectid) {
   console.log('starting ' + projectid);
@@ -63,86 +128,6 @@ async function stop_bot(projectid) {
     console.error(`Error stopping bot: ${error}`);
   }
 };
-
-// Keep track of running external processes for bots.
-let running_bots = {};
-
-const app = express();
-
-// Set up server for HTTP/HTTPS
-let server = null;
-let port = parseInt(process.env.LISTEN_PORT ?? '0');
-
-if (fs.existsSync(__dirname + '/certs/privkey.pem') && fs.existsSync(__dirname + '/certs/pubkey.pem')) {
-
-  const key = fs.readFileSync(__dirname + '/certs/privkey.pem');
-  const cert = fs.readFileSync(__dirname + '/certs/pubkey.pem');
-  var ssloptions = {
-    key: key,
-    cert: cert
-  };
-
-  port ||= 443;
-  server = https.createServer(ssloptions, app);
-} else {
-  port ||= 80;
-  server = http.createServer(app);
-}
-
-// parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-// parse application/json
-app.use(express.json());
-
-// For CORS
-app.use(cors({
-  allowedHeaders: ['Content-Type'],
-  origin: 'http://localhost:5173',
-  preflightContinue: true,
-  credentials: true,
-}));
-
-// Set up the MongoDB connection
-const dbPath = process.env.MONGO_USERNAME
-  ? `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@mongo:${process.env.MONGO_PORT ?? 27017}/${process.env.MONGO_DB ?? 'tilbot'}`
-  : (process.env.MONGO_DB ?? 'mongodb://127.0.0.1:27017/tilbot');
-
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
-await mongoose.connect(dbPath, options);
-// Make sure the express-mongodb-session can also use the existing connection
-console.log(mongodbsession);
-const MongoDBStore = mongodbsession(session);
-
-// Sessions
-const store = new MongoDBStore({
-  // Because we are using mongoose rather than MongoDB, express-mongodb-session refuses to use the existing connection because it is not instanceof MongoDB.MongoClient
-  //existingConnection: mongo.connection,
-  //databaseName: process.env.MONGO_DB,
-  uri: dbPath,
-  collection: 'sessions'
-});
-
-// Catch errors
-store.on('error', function (error) {
-  console.log(error);
-});
-
-app.use(session({
-  secret: 'Super secretthings',
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
-  },
-  store: store,
-  // Boilerplate options, see:
-  // * https://www.npmjs.com/package/express-session#resave
-  // * https://www.npmjs.com/package/express-session#saveuninitialized
-  resave: true,
-  saveUninitialized: true,
-  secure: process.env.HTTPS ? true : false,
-  httpOnly: true,
-  sameSite: 'none'
-}));
 
 // Launch all running bots (IIFE to run it in the background)
 (async () => {
@@ -311,68 +296,64 @@ app.post('/api/set_project_status', async (req, res) => {
 /**
  * API call: Import a project
  */
-app.post('/api/import_project', upload.single('file'), async (req, res) => {
-  try {
-    // Source: https://medium.com/@ritikkhndelwal/getting-the-data-from-the-multipart-form-data-in-node-js-dc2d99d10f97
+app.post('/api/import_project', async (req, res) => {
+  // Source: https://medium.com/@ritikkhndelwal/getting-the-data-from-the-multipart-form-data-in-node-js-dc2d99d10f97
 
-    console.log('=== IMPORT PROJECT ===');
-    console.log(req.body);
+  console.log('=== IMPORT PROJECT ===');
+  console.log(req.body);
 
-    const project_id = req.body.project_id;
+  const project_id = req.body.project_id;
 
-    // Be extra careful because we're going to do filesystem operations here!
-    if (!/^[0-9a-f]{32}$/.test(project_id)) {
-      throw new TilBotProjectNotFoundError(project_id);
-    }
-
-    // Ensure that the project exists and is owned by the user:
-    const project = await ProjectModel.getById(project_id, { user_id: req.session.username });
-
-    const priv_dir = 'projects/' + project_id;
-    const pub_dir = 'proj_pub/' + project_id;
-
-    fs.rmSync(priv_dir, { recursive: true, force: true });
-    fs.mkdirSync(priv_dir, { recursive: true });
-
-    fs.rmSync(pub_dir, { recursive: true, force: true });
-    fs.mkdirSync(pub_dir, { recursive: true });
-
-    const zip = new AdmZip(req.file.path);
-    const zipEntries = zip.getEntries(); // an array of ZipEntry records
-
-    console.log(zipEntries);
-
-    let project_data = null;
-
-    zipEntries.forEach(zipEntry => {
-      if (zipEntry.entryName == "project.json") {
-        project_data = zipEntry.getData().toString("utf8");
-
-        // @TODO: import project file into database
-        //win.webContents.send('project-load', zipEntry.getData().toString("utf8"));
-      } else if (zipEntry.entryName.startsWith('var/')) {
-        zip.extractEntryTo(zipEntry, priv_dir);
-      } else {
-        zip.extractEntryTo(zipEntry, pub_dir);
-      }
-    });
-
-    if (project_data == null) {
-      throw new TilBotNoProjectFileError();
-    }
-
-    console.log('project file found');
-
-    if (running_bots[project_id] !== undefined) {
-      await stop_bot(project_id);
-    }
-
-    project.fromModel(JSON.parse(project_data));
-    await project.save();
-  } finally {
-    // Remove the temporary file
-    fs.rmSync(req.file.path);
+  // Be extra careful because we're going to do filesystem operations here!
+  if (!/^[0-9a-f]{32}$/.test(project_id)) {
+    throw new TilBotProjectNotFoundError(project_id);
   }
+
+  // Ensure that the project exists and is owned by the user:
+  const project = await ProjectModel.getById(project_id, { user_id: req.session.username });
+
+  const priv_dir = 'projects/' + project_id;
+  const pub_dir = 'proj_pub/' + project_id;
+
+  rmSync(priv_dir, { recursive: true, force: true });
+  mkdirSync(priv_dir, { recursive: true });
+
+  rmSync(pub_dir, { recursive: true, force: true });
+  mkdirSync(pub_dir, { recursive: true });
+
+  const zip = new AdmZip(req.raw.files.file.tempFilePath);
+  const zipEntries = zip.getEntries(); // an array of ZipEntry records
+
+  console.log(zipEntries);
+
+  let project_data = null;
+
+  zipEntries.forEach(zipEntry => {
+    if (zipEntry.entryName == "project.json") {
+      project_data = zipEntry.getData().toString("utf8");
+
+      // @TODO: import project file into database
+      //win.webContents.send('project-load', zipEntry.getData().toString("utf8"));
+    } else if (zipEntry.entryName.startsWith('var/')) {
+      zip.extractEntryTo(zipEntry, priv_dir);
+    } else {
+      zip.extractEntryTo(zipEntry, pub_dir);
+    }
+  });
+
+  if (project_data == null) {
+    throw new TilBotNoProjectFileError();
+  }
+
+  console.log('project file found');
+
+  if (running_bots[project_id] !== undefined) {
+    await stop_bot(project_id);
+  }
+
+  project.fromModel(JSON.parse(project_data));
+  await project.save();
+
   res.send('OK');
 });
 
@@ -427,7 +408,7 @@ app.get('/api/sesh', (req, res) => {
 });
 
 // In production, let SvelteKit handle everything else, including serving prerendered pages and static assets
-if (fs.existsSync('./build/handler.js')) {
+if (existsSync('./build/handler.js')) {
   (async () => {
     try {
       const m = await import('./build/handler.js');
@@ -439,13 +420,13 @@ if (fs.existsSync('./build/handler.js')) {
   })();
 }
 
-// Must be last in order to catch errors from all routes
-app.use((err, req, res, next) => {
-  if (res.headersSent) {
-    // Not much we can do now that (some) data has already been sent.
+app.setErrorHandler((err, req, res) => {
+  if (res.sent) {
+    // Not much we can do now that data has already been sent.
     console.log(err);
     next();
   } else if (err instanceof TilBotError) {
+    res.status(200);
     res.send(err.api_status_code);
   } else {
     console.log(err);
@@ -453,6 +434,13 @@ app.use((err, req, res, next) => {
   }
 });
 
-server.listen(port, '0.0.0.0', () => {
-  console.log('listening on port ' + port);
+const port = parseInt(process.env.LISTEN_PORT || '8000');
+
+app.listen({ port, host: '0.0.0.0' }, (err, addr) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  } else {
+    console.log(`listening on ${addr}`);
+  }
 });
