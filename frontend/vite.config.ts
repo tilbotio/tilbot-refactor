d2a59a3612f0266edc51b3058050e58ec763c655
Paul van Geldrop <git@paulvangeldrop.nl>
2025-05-23 11:58:41 +0200

    Add new tailwindcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,27 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-const API_URL = process.env.VITE_API_URL || 'http://localhost:8000';
+const API_URL = process.env.VITE_API_URL || "http://localhost:8000";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), tailwindcss()],
   server: {
     host: true,
     allowedHosts: true,
     // port: 5173,
     // strictPort: true,
     fs: {
-      allow: ['proj_pub']
+      allow: ["proj_pub"],
     },
     proxy: {
-      '/ws': API_URL.replace(/^http(s?):/, 'ws$1:'),
-      '/api': {
+      "/ws": API_URL.replace(/^http(s?):/, "ws$1:"),
+      "/api": {
         target: API_URL,
         changeOrigin: true,
         secure: false,
         // rewrite: (path) => path.replace(/^\/api/, '')
       },
-    }
-  }
+    },
+  },
 });
