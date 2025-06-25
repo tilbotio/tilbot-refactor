c01c0f9392135f1ced7661a1af51c3dae33038eb
Paul van Geldrop <git@paulvangeldrop.nl>
2025-06-25 17:07:20 +0200

    Add data loading to +page.ts

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,4 +6,5 @@
 
 <div class="w-full h-full bg-gray-50">
   <h1>Showheader property is {data.showHeader}</h1>
+  <p>Data received from +page.ts: {data}</p>
 </div>
--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,9 +1,47 @@
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ url, fetch }) => {
-  const showHeaderParam = url.searchParams.get("show_header");
-  const showHeader = showHeaderParam !== null ? showHeaderParam === "1" : true;
+  const showHeaderParam = url.searchParams.get("show_header") ?? "1";
+  const participantId = url.searchParams.get("pid") || null;
+  const projectId = url.searchParams.get("project") || null;
+  const showHeader = showHeaderParam === "1";
+  let path: string = "";
+  let conversationId: string | null = null;
+  let settings: any = {
+    typing_style: "fixed",
+    typing_time: 2,
+    typing_charpsec: 40,
+    show_avatar: "yes",
+    avatar_file: "",
+    name: "Tilbot",
+  };
+
+  if (projectId) {
+    path = `/proj_pub/${projectId}`;
+    try {
+      const response = await fetch(
+        `/api/create_conversation?id=${encodeURIComponent(projectId)}`
+      );
+
+      if (response.ok) {
+        ({ conversation: conversationId, settings } = await response.json());
+        console.log(settings);
+      } else {
+        throw new Error(
+          `Invalid response: ${response.status} ${response.statusText}`
+        );
+      }
+    } catch (err: any) {
+      console.error(`Failed to fetch conversation data: ${err.message}`);
+    }
+  }
+
   return {
     showHeader,
+    participantId,
+    path,
+    projectId,
+    settings,
+    conversationId,
   };
 };
