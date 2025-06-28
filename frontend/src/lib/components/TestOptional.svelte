d9335951d60622979293c961c3a26420aafad59d
Paul van Geldrop <git@paulvangeldrop.nl>
2025-06-28 16:53:02 +0200

    Adjust interfaces to allow for optional parameters in order to pass subsets to children while maintaining typing

---
+++ frontend/src/lib/components/TestOptional.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import type { RuntimeContext } from "$lib/types/RuntimeContext";
+
+    let { path, participantId }: RuntimeContext = $props();
+</script>
+
+<h1>Awesome, we know only get path (which is {path}) and participantId (which is null, but who cares)</h1>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,6 +6,7 @@
   import Test from "$lib/components/Test.svelte";
   import { setContext } from "svelte";
   import type { RuntimeContext } from "$lib/types/RuntimeContext";
+  import TestOptional from "$lib/components/TestOptional.svelte";
 
   let { data: loadResult }: PageProps = $props();
   setContext("settings-context", loadResult.settings);
@@ -16,3 +17,5 @@
 <!--<ChatView {...loadResult} />-->
 <ChatHeader2 showHeader = {loadResult.showHeader} runtimeContext = {loadResult.runtimeContext} settings= {loadResult.settings}/>
 <Test />
+<!--And here we pass only a few properties of runtimeContext to check if optional parameters can be helpful in this project-->
+<TestOptional path={loadResult.runtimeContext.path} participantId={loadResult.runtimeContext.participantId} />
