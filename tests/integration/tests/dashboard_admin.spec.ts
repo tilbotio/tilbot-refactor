6448dad53812fa403d7062206c07462aa443ca10
Jan de Wit <jan@tilbot.io>
2025-07-01 14:41:58 +0200

    Moved to a docker-compose strategy for mongo

--- tests/integration/tests/dashboard_admin.spec.ts
+++ tests/integration/tests/dashboard_admin.spec.ts
@@ -14,3 +14,18 @@ test('login button 2', async ({ page }) => {
     await expect(page.getByText('Login')).toBeVisible();
 });
 
+test('login admin', async ({ page }) => {
+    // @TODO: Empty DB
+    
+    await page.goto('http://localhost:5173/login/');
+
+    //await page.getByRole("textbox", {name: "username"}).fill('admin');
+    //await page.getByRole("textbox", {name: "password"}).fill('admin');
+    await page.locator("#txt_username").fill('admin');
+    await page.locator("#txt_pass").fill('admin');
+
+    await page.getByRole("button").click();
+
+    // Find login button.
+    await expect(page.getByText('Welcome, admin!')).toBeVisible();
+});
--- tests/integration/tests/global.setup.ts
+++ tests/integration/tests/global.setup.ts
@@ -1,25 +1,17 @@
 import { test as setup } from '@playwright/test';
 import { DockerComposeEnvironment, Wait } from 'testcontainers';
-import { MongoDBContainer } from '@testcontainers/mongodb';
-
-setup('start test mongo', async ({ }) => {
-    const mongodbContainer = await new MongoDBContainer("mongo:5").withReuse().start();
-});
 
 setup('start containers', async ({ }) => {
     setup.setTimeout(120_000);
     console.log('starting containers...');
 
     // Start the Docker containers
-    const composeFilePath = "../../";
+    const composeFilePath = ".";
     const composeFile = "docker-compose.yml";
     
-    //const environment = await new DockerComposeEnvironment(composeFilePath, composeFile).withWaitStrategy("frontend-1", Wait.forLogMessage(".*Local.*")).up(["frontend", "backend"]); 
-    
-    // I cannot get the wait strategies to work, so I have a hard-coded 60 sec. delay for the Docker containers to spin up now.
-    const environment = new DockerComposeEnvironment(composeFilePath, composeFile).up(["frontend", "backend"]); 
-    const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-    await delay(60000);
+    const environment = await new DockerComposeEnvironment(composeFilePath, composeFile).withWaitStrategy('frontend-1', Wait.forListeningPorts()).up(); 
+    //const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+    //await delay(60000);
 
     console.log('loaded!');
 
