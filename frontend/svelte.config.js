46ff46a90596b6c8569a1fe82e1f98c2e09e5670
Paul van Geldrop <git@paulvangeldrop.nl>
2025-07-01 16:12:39 +0200

    Set compilerOptions runes to true for project to force Runes mode

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,6 +3,7 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	compilerOptions: {runes: true},
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess({
