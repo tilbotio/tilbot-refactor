a174be9676049f47e6cf87ac32fd4a982b4d8316
Paul van Geldrop <git@paulvangeldrop.nl>
2025-07-07 14:53:08 +0200

    Add empty InputArea.svelte component

---
+++ frontend/src/lib/components/InputArea.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { PaperAirplane, Plus, QrCode } from "svelte-heros-v2";
+  type Props = {
+    inputMode: "text" | "mc";
+  };
+
+  const { inputMode }: Props = $props();
+</script>
