2b9a061c71dfda003925d7f75448938e2d5dc47d
Paul van Geldrop <git@paulvangeldrop.nl>
2025-07-07 13:08:32 +0200

    Add alignment to chatmessages based on message.from

--- frontend/src/lib/components/ChatMessage.svelte
+++ frontend/src/lib/components/ChatMessage.svelte
@@ -4,15 +4,17 @@
   function determineBackground(author: string) {
     switch (author) {
       case "bot":
-        return "bg-tilbot-secondary-purple";
+        return ["bg-tilbot-secondary-purple", "chat-start"];
       case "chatgpt":
-        return "bg-[#FFC500]";
+        return ["bg-[#FFC500]", "chat-end"];
       default:
-        return "bg-tilbot-secondary-hardpink";
+        return ["bg-tilbot-secondary-hardpink", "chat-end"];
     }
   }
 
-  const background: string = determineBackground(message.from);
+  const [background, alignment] = determineBackground(message.from);
 </script>
 
-<div class="chat-bubble {background}">{@html message.content}</div>
+<div class="chat {alignment}">
+  <div class="chat-bubble {background}">{@html message.content}</div>
+</div>
