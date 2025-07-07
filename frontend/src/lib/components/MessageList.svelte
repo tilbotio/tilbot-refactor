76ed4a7dcc3c57f372fb2353edbaa62e48fb8df9
Paul van Geldrop <git@paulvangeldrop.nl>
2025-07-07 14:31:27 +0200

    Change comment text

--- frontend/src/lib/components/MessageList.svelte
+++ frontend/src/lib/components/MessageList.svelte
@@ -5,7 +5,7 @@
 </script>
 
 {#each messages as message, index}
-  <!-- if this is the start of a new message block for the bot, ChatMessage should be prompted to display the bot avatar-->
+  <!-- if this is the start of a new message block for the bot, ChatMessage should be told to display the bot avatar-->
   {@const newBotMessageBlock: boolean =
     message.from == "bot" && (index == 0 || messages[index - 1].from != "bot")}
 
