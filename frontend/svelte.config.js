53d9af268bf28c888e3f9bc70d96aa5de3b81921
Jan de Wit <jan@neryana.com>
2025-07-30 11:26:25 +0200

    Simplified environment check and added new import style for Svelte 6 (commented)

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,18 +1,21 @@
 import adapterNode from "@sveltejs/adapter-node";
 import adapterStatic from "@sveltejs/adapter-static";
-import preprocess from "svelte-preprocess";
+import sveltePreprocess from "svelte-preprocess";
+
+// For after upgrading to Svelte v6
+//import { sveltePreprocess } from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess({
+  preprocess: sveltePreprocess({
     postcss: true,
   }),
 
   kit: {
     adapter:
-      process.env.TILBOT_ENV !== undefined && process.env.TILBOT_ENV == "app"
+      process.env.TILBOT_ENV === "app"
         ? adapterStatic({ pages: "../app/build" })
         : adapterNode(),
   },
