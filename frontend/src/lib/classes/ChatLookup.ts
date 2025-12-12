4c9e2b738d3abeec90e0bf3d656402aafc8e9603
Paul van Geldrop <git@paulvangeldrop.nl>
2025-12-12 11:22:28 +0100

    Add ChatLookup class

--- frontend/src/lib/classes/ChatLookup.ts
+++ frontend/src/lib/classes/ChatLookup.ts
@@ -1,17 +1,40 @@
 import type { ProjectControllerLookupInterface } from "../../../../common/projectcontroller/types";
 
 export class ChatLookup implements ProjectControllerLookupInterface {
-    async cell(db: string, col: string, val: string): Promise<Object[] | null> {
-        // Not used in the chatinterface
-        return null     
-    }
+    private promise!: Promise<string>;
+    private pendingResolver: ((value: string) => void) | null = null;
+  async cell(db: string, col: string, val: string): Promise<Object[] | null> {
+    // Not used in the chatinterface
+    return null;
+  }
 
-    async random(db: string): Promise<Object | null> {
-        // Not used in the chatinterface
-        return null;
-    }
+  async random(db: string): Promise<Object | null> {
+    // Not used in the chatinterface
+    return null;
+  }
 
-    async variation(content: string, prompt: string, memory: any): Promise<string> {
-        return await "null"
+  async variation(
+    content: string,
+    prompt: string,
+    memory: any
+  ): Promise<string> {
+    window.parent.postMessage({
+      msg: "variation",
+      content: content,
+      prompt: prompt,
+      memory: memory,
+    });
+
+    this.promise = new Promise((resolve) => {
+      this.pendingResolver = resolve;
+    });
+    return await this.promise;
+  }
+
+  resolveVariation(result: string) {
+    if (this.pendingResolver) {
+        this.pendingResolver(result);
+        this.pendingResolver = null;
     }
+  }
 }
